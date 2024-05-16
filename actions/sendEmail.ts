a8a6dc4c3716047172e082de5ab4851fe0d9fa7a
Ajinkya-Kardile <kardileajinkya@gmail.com>
2024-05-16 22:56:08 +0530

    test

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,9 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_8hhRNWFm_5dycqU5uWTzoAvPDJQwP9a48");
+
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
